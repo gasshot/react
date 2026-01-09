2ccdf1b352579a045853deea6c33d40073291dfe
YYS-KJCA <yys0229@gmail.com>
2026-01-09 16:58:42 +0900

    260109 1658

--- chapter8-e-commerce/src/components/Home/index.jsx
+++ chapter8-e-commerce/src/components/Home/index.jsx
@@ -1,34 +1,53 @@
 import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router'
 import styles from './Home.module.css'
+import useFetchProducts from '../../hooks/useFetchProducts';
 
 const Home = () => {
+  const { products } = useFetchProducts();
+  const [selectedCategory, setSelectedCategory] = useState("전체");
 
-  const products = Array(10).fill(null);
+  const filteredProducts = products.filter((category)=>{
+    if(selectedCategory ==="전체"){
+      return true;
+    }
+    return selectedCategory === category;
+  })
+
   return (
     <>
       <ul className={styles.categoryList}>
-        <li className={styles.selected}>전체</li>
-        <li>상의</li>
-        <li>하의</li>
-        <li>신발</li>
-        <li>가방</li>
-        <li>악세서리</li>
+        {["전체", "상의", "하의", "신발", "악세서리"].map((category) => {
+          return <li key={category} className={
+            selectedCategory === category ? styles.selected : null}
+            onClick={() => {
+              setSelectedCategory(category);
+            }}>
+            {category}
+          </li>
+        })}
       </ul>
 
-      <h3>상품 목록(20)</h3>
+      <h3>상품 목록({products.length})</h3>
       <div className={styles.productList}>
-        {products.map((_, index) => (
-          <div className={styles.productListItem} key={index}>
-            <img src="https://picsum.photos/200" alt="" />
-            <div className={styles.productInfo}>
-              <div className={styles.productCategory}>상의</div>
-              <div className={styles.productName}>멋진 니트</div>
-              <div className={styles.productPrice}>19,000원</div>
-            </div>
-          </div>
-        ))}
-      </div>
+        {filteredProducts.map(({ id, category, image, name, price }) => {
 
+          return (
+            <Link key={id} to={`/products/${id}`} className={styles.productListItem}>
+              <div >
+                <img src={image} />
+                <div className={styles.productInfo}>
+                  <div className={styles.productCategory}>{category}</div>
+                  <div className={styles.productName}>{name}</div>
+                  <div className={styles.productPrice}>{price.toLocaleString()}원</div>
+                </div>
+              </div>
+
+            </Link>
+          )
+        })}
+      </div>
     </>
   )
 }
